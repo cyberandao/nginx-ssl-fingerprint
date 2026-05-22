571d7703bfb30d8637921d068fc598ffeb6d5253
jimmy <goodmannum1@protonmail.com>
2026-05-22 04:44:26 +0000

    fix: add null checks for memory allocation in ngx_ssl_ja3 and ngx_http2_fingerprint functions

--- src/nginx_ssl_fingerprint.c
+++ src/nginx_ssl_fingerprint.c
@@ -211,6 +211,10 @@ int ngx_ssl_ja3(ngx_connection_t *c)
 
     c->ssl->fp_ja3_str.len = c->ssl->fp_ja3_data.len * 3;
     c->ssl->fp_ja3_str.data = ngx_pnalloc(c->pool, c->ssl->fp_ja3_str.len);
+    if (c->ssl->fp_ja3_str.data == NULL) {
+        c->ssl->fp_ja3_str.len = 0;
+        return NGX_DECLINED;
+    }
 
     ngx_log_debug(NGX_LOG_DEBUG_EVENT, c->log, 0, "ngx_ssl_ja3: alloc bytes: [%d]\n", c->ssl->fp_ja3_str.len);
 
@@ -313,6 +317,10 @@ int ngx_ssl_ja3_hash(ngx_connection_t *c)
 
     c->ssl->fp_ja3_hash.len = 32;
     c->ssl->fp_ja3_hash.data = ngx_pnalloc(c->pool, c->ssl->fp_ja3_hash.len);
+    if (c->ssl->fp_ja3_hash.data == NULL) {
+        c->ssl->fp_ja3_hash.len = 0;
+        return NGX_DECLINED;
+    }
 
     ngx_log_debug(NGX_LOG_DEBUG_EVENT, c->log, 0, "ngx_ssl_ja3_hash: alloc bytes: [%d]\n", c->ssl->fp_ja3_hash.len);
 
@@ -341,46 +349,65 @@ int ngx_http2_fingerprint(ngx_connection_t *c, ngx_http_v2_connection_t *h2c)
         return NGX_OK;
     }
 
+    if (h2c->fp_settings.len % 5 != 0 || h2c->fp_priorities.len % 4 != 0) {
+        return NGX_DECLINED;
+    }
+
     n = 4 + h2c->fp_settings.len * 3 + 10 + h2c->fp_priorities.len * 2 + h2c->fp_pseudoheaders.len * 2;
     h2c->fp_str.data = ngx_pnalloc(c->pool, n);
+    if (h2c->fp_str.data == NULL) {
+        h2c->fp_str.len = 0;
+        return NGX_DECLINED;
+    }
+
     pstr = h2c->fp_str.data;
 
     ngx_log_debug(NGX_LOG_DEBUG_EVENT, c->log, 0, "ngx_http2_fingerprint: alloc bytes: [%d]\n", n);
 
     /* setting */
-    for (i = 0; i < h2c->fp_settings.len; i+=5) {
-        pstr = append_uint8(pstr, h2c->fp_settings.data[i]);
-        *pstr++ = ':';
-        pstr = append_uint32(pstr, *(uint32_t*)(h2c->fp_settings.data+i+1));
-        *pstr++ = ';';
+    if (h2c->fp_settings.len > 0) {
+        for (i = 0; i < h2c->fp_settings.len; i+=5) {
+            pstr = append_uint8(pstr, h2c->fp_settings.data[i]);
+            *pstr++ = ':';
+            pstr = append_uint32(pstr, *(uint32_t*)(h2c->fp_settings.data+i+1));
+            *pstr++ = ';';
+        }
+        *(pstr-1) = '|';
+    } else {
+        *pstr++ = '|';
     }
-    *(pstr-1) = '|';
 
     /* windows update */
     pstr = append_uint32(pstr, h2c->fp_windowupdate);
     *pstr++ = '|';
 
     /* priorities */
-    for (i = 0; i < h2c->fp_priorities.len; i+=4) {
-        pstr = append_uint8(pstr, h2c->fp_priorities.data[i]);
-        *pstr++ = ':';
-        pstr = append_uint8(pstr, h2c->fp_priorities.data[i+1]);
-        *pstr++ = ':';
-        pstr = append_uint8(pstr, h2c->fp_priorities.data[i+2]);
-        *pstr++ = ':';
-        pstr = append_uint16(pstr, (uint16_t)h2c->fp_priorities.data[i+3]+1);
-        *pstr++ = ',';
+    if (h2c->fp_priorities.len > 0) {
+        for (i = 0; i < h2c->fp_priorities.len; i+=4) {
+            pstr = append_uint8(pstr, h2c->fp_priorities.data[i]);
+            *pstr++ = ':';
+            pstr = append_uint8(pstr, h2c->fp_priorities.data[i+1]);
+            *pstr++ = ':';
+            pstr = append_uint8(pstr, h2c->fp_priorities.data[i+2]);
+            *pstr++ = ':';
+            pstr = append_uint16(pstr, (uint16_t)h2c->fp_priorities.data[i+3]+1);
+            *pstr++ = ',';
+        }
+        *(pstr-1) = '|';
+    } else {
+        *pstr++ = '|';
     }
-    *(pstr-1) = '|';
 
     /* fp_pseudoheaders */
-    for (i = 0; i < h2c->fp_pseudoheaders.len; i++) {
-        *pstr++ = h2c->fp_pseudoheaders.data[i];
-        *pstr++ = ',';
+    if (h2c->fp_pseudoheaders.len > 0) {
+        for (i = 0; i < h2c->fp_pseudoheaders.len; i++) {
+            *pstr++ = h2c->fp_pseudoheaders.data[i];
+            *pstr++ = ',';
+        }
+        *--pstr = 0;
+    } else {
+        *pstr = 0;
     }
-
-    /* null terminator */
-    *--pstr = 0;
 
     h2c->fp_str.len = pstr - h2c->fp_str.data;
 
